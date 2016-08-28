57bf45329c2fa2422872050de1ee3c345b633849
Frank Iriarte <jurrish@gmail.com>
2016-08-27 17:45:27 -0700

    touched up server side get request/response

--- scripts/repo.js
+++ scripts/repo.js
@@ -1,14 +1,19 @@
 (function(module) {
   var repos = {};
   repos.all = [];
-
+//client side get request
+//difference between ajax and GET:
+//ajax has more options to send a request(any CRUD operation, GET, POST, DELETE), but when it's used, you have to fill on the type, headers, etc (if they're needed -- ie: check the docs);
+//$.get/GET doesn't require those options.
   repos.requestRepos = function(callback) {
     $.ajax({
       url: '/github/user/jurrish/repos?sort=updated&per_page=10',
-      type: 'GET',
-      headers: {
-        Authorization: 'token ' + GITHUB_TOKEN
-      }
+      type: 'GET'
+      //-----------------------------
+      // headers: {
+      //   Authorization: 'token ' + GITHUB_TOKEN
+      // } -----------------------
+      //dont need this part because this is just making a request to the server i built (and doesnt need authorization) BUT it will need authorization when the server itself makes a request to the githup API
     }).done(function(data) {
       console.log(data);
       repos.all = data;
